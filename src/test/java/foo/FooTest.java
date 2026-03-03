ebb13bf7d95feaf2a623a693ad7a1dbaa2348170
gourab dash <nddashcan@gmail.com>
2026-03-03 21:42:14 +0000

    a

--- src/test/java/foo/FooTest.java
+++ src/test/java/foo/FooTest.java
@@ -9,4 +9,47 @@ public class FooTest {
   public void testAdd() throws Exception {
 	assertEquals(2, Foo.div(10, 5));
   }
+  
+}package foo;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class FooTest {
+
+  @Test
+  public void testDiv() throws Exception {
+    assertEquals(2, Foo.div(10, 5));
+  }
+
+  @Test
+  public void testDivReturnsOne() throws Exception {
+    assertEquals(1, Foo.div(5, 5));
+  }
+
+  @Test
+  public void testDivWithNegativeDividend() throws Exception {
+    assertEquals(-2, Foo.div(-10, 5));
+  }
+
+  @Test
+  public void testDivWithNegativeDivisor() throws Exception {
+    assertEquals(-2, Foo.div(10, -5));
+  }
+
+  @Test
+  public void testDivWithBothNegative() throws Exception {
+    assertEquals(2, Foo.div(-10, -5));
+  }
+
+  @Test
+  public void testDivWithZeroDividend() throws Exception {
+    assertEquals(0, Foo.div(0, 5));
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void testDivByZeroThrowsException() throws Exception {
+    Foo.div(10, 0);
+  }
+
 }
